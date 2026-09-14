fa8499c8dc77b44a0cba79f8aa9168b698d2e436
服部歩希 <ayumaru0711@gmail.com>
2026-09-15 02:16:57 +0900

    Change redirection from next.html to select.html

--- assets/js/start.js
+++ assets/js/start.js
@@ -4,5 +4,5 @@ const startBtn = document.getElementById('addRoutineBtn');
 // ボタンがクリックされた時の処理
 startBtn.addEventListener('click', function() {
     // 遷移したいページのURLまたはファイル名を入力
-    window.location.href = 'next.html'; 
+    window.location.href = 'select.html'; 
 });
